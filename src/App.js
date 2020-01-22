f60af35e12be78d3ec5cb956d574d19a56acdd44
jennajubeir <48401314+jennajubeir@users.noreply.github.com>
2020-01-21 21:41:23 -0600

    --authenticationworking

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import ProductGridView from "./components/ProductGridView";
 import ProductCard from "./components/ProductCard";
 import "./components/ProductCard.css";
 import CartDisplay from "./components/Cart";
+import Button from "@material-ui/core/Button";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import "rbx/index.css";
+import { Title, Message } from "rbx";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBS4qjaorkbIIy3Xxsv6_p9TYotcjs2Mlw",
@@ -18,6 +22,36 @@ const firebaseConfig = {
   appId: "1:303319913712:web:c50706ebce8652217d9e35"
 };
 
+require("firebase/auth");
+require("firebase/firestore");
+
+const uiConfig = {
+  signInFlow: "popup",
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: () => false
+  }
+};
+
+const SignIn = () => (
+  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+);
+
+const Welcome = ({ user }) => (
+  <Message color="info">
+    <Message.Header>
+      Welcome, {user.displayName}
+      <Button primary onClick={() => firebase.auth().signOut()}>
+        Log out
+      </Button>
+    </Message.Header>
+  </Message>
+);
+
+const Banner = ({ user, title }) => (
+  <React.Fragment>{user ? <Welcome user={user} /> : <SignIn />}</React.Fragment>
+);
+
 firebase.initializeApp(firebaseConfig);
 
 const App = () => {
@@ -26,6 +60,11 @@ const App = () => {
   const [itemsInCart, setCartProducts] = useState({});
   const [cartPrice, setCartPrice] = useState(0);
   const [inventory, setInventory] = useState({});
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser);
+  }, []);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -46,6 +85,7 @@ const App = () => {
   return (
     <div>
       <p className="title-page"> JENNA'S CLOTHING COMPANY</p>
+      <Banner user={user}> </Banner>
       <ProductGridView
         products={products}
         itemsInCart={itemsInCart}
